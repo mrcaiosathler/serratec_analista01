678695200533b6936125655b6bcc630aef15b721
Caio Sathler de Lima <108816284+mrcaiosathler@users.noreply.github.com>
2025-05-22 10:31:47 -0300

    Atualização e organização dos arquivos

---
+++ python_analista01/monitoria0426.py
@@ -0,0 +1,7 @@
+# Exemplo 2:
+nome = input("Digite seu nome: ")
+while nome=='Pedro':
+    if nome == 'Pedro':
+        print("Você não pode usar esse nome")
+    else:
+        print(f"Olá {nome}")
